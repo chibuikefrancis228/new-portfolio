3fd8e710c9c7ff6af5571ac30bb3504ce2421573
chibuikefrancis228 <chibuikefrancis228@gmail.com>
2025-02-03 18:23:24 -0800

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+export const dynamic = 'force-dynamic'
+import { ThemeProvider } from "./provider";
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-ibm-plex-serif'
+})
+
+export const metadata: Metadata = {
+  title: "portfolio",
+  description: "Horizon is a modern banking platform for everyone.",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider> 
+      
+      </body>
+    </html>
+  );
+}
